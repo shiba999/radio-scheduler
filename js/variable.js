237c481c541c1a339ec5dfe732a5b38c908b6cac
Shibazaki <groove.groove.999@gmail.com>
2026-01-10 00:05:21 +0900

    20260110 add file
    Signed-off-by: Shibazaki <groove.groove.999@gmail.com>

---
+++ js/variable.js
@@ -0,0 +1,34 @@
+
+
+	// 使用する要素群
+
+	export const e = {
+		controller: document.getElementById("controller"),
+		sche_list: document.getElementById("schedule_list"),
+		audio_list: document.getElementById("audio_file_list"),
+		up_zone: document.getElementById("upload"),
+		up_msg: document.getElementById("upload_message"),
+		radio_info: document.getElementById("play_info"),
+		radio_stop: document.getElementById("radio_stop"),
+		vol_range: document.getElementById("volume_range"),
+		reboot: document.getElementById("system_reboot"),
+		shutdown: document.getElementById("system_power_off"),
+		area_name: document.getElementById("area_name"),
+		station_list: document.getElementById("station_list"),
+		xml_info: document.getElementById("xml_info"),
+		option_open: document.getElementById("option_open_btn"),
+		option_box: document.getElementById("option_nemu_box"),
+		s_reload: document.getElementById("super_reload")
+	}
+
+
+	// 使用する変数群
+
+	export const v = {
+		region: false,
+		channels: [],
+		audio_files: [],
+		audio_play: false,
+		set_interval: []
+	}
+
